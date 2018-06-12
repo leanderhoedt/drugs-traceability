b73b6aec3a05c6742bd51a23f3d25c0d91c6e924
leander.hoedt <leander.hoedt@gmail.com>
2018-06-12 17:59:28 +0200

    changed upgradeBusinessNetwork script with version as string

--- upgradeBusinessNetwork.sh
+++ upgradeBusinessNetwork.sh
@@ -1,4 +1,4 @@
-version=0.0.2
+version="0.0.2"
 composer archive create -t dir -n .
 composer network install --card PeerAdmin@hlfv1 --archiveFile drug_network@$version.bna
 composer network upgrade --card PeerAdmin@hlfv1 -n drug_network -V $version
