4d7b807a9bcc9d2b13cd44971626f7914e16a320
leander.hoedt <leander.hoedt@gmail.com>
2018-06-13 16:48:00 +0200

    Fixed startBusinessNetwork script

--- startBusinessNetwork.sh
+++ startBusinessNetwork.sh
@@ -1,5 +1,5 @@
 # ./startFabrich.sh --> is fabric already started ??
 # ./createPeerAdmin.sh --> is PeerAdmin card already generated??
 composer archive create -t dir -n .
-composer network install --archiveFile drug_network@1.0.0.bna --card PeerAdmin@hlfv1
-composer network start --networkName drug_network --networkVersion 1.0.0 --card PeerAdmin@hlfv1 --networkAdmin admin --networkAdminEnrollSecret adminpw
+composer network install --archiveFile drug_network@0.0.1.bna --card PeerAdmin@hlfv1
+composer network start --networkName drug_network --networkVersion 0.0.1 --card PeerAdmin@hlfv1 --networkAdmin admin --networkAdminEnrollSecret adminpw
