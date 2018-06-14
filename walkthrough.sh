5e2f46400d0e8d8fa37e2b1806bb014a87ab16c9
leander.hoedt <leander.hoedt@gmail.com>
2018-06-14 18:16:55 +0200

    Fixed manufacturer create drugs permission

--- walkthrough.sh
+++ walkthrough.sh
@@ -45,12 +45,47 @@ pharmacistName="pharmacist_$pharmacistId"
 patientId="cedric"
 patientName="patient_$patientId"
 
+echo "Login as Manufacturer ... (or just continue as admin)"
+echo "create new bash and run: composer-rest-server -a 'never' -c manufacturer_jo@drug_network"
+read -p
 echo "Creating drug ..."
+# create drug
 curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "$class": "org.drugs.CreateDrug", "drugHash": "'$dafalganDrugHash'", "name": "Dafalgan", "metaData": { "$class": "org.drugs.DrugMetaData", "serialNumber": "'$dafalganSerialNumber'", "productCode": "'$dafalganProductCode'", "batchNumber": "'$dafalganBatchNumber'", "manufacturer": "resource:org.drugs.Manufacturer#'$manufacturerId'" } }' "$URI/api/CreateDrug"
 
 echo ""
 echo "Created drug '$dafalganDrugHash'"
 echo "by Manufacturer '$manufacturerId'"
 
+echo "Login as Distributer ..."
+echo "create new bash and run: composer-rest-server (distributer_gene@drug_network)"
+read -p
 echo ""
 echo "Distributer '$distributerId' receives drugs from manufacturer '$manufacturerId'"
+
+# receive drug from manufacturer to distributer
+curl
+
+
+echo "Received drug from Manufacturer '$manufacturerId'"
+echo "to Distributer '$distributerId'"
+
+echo "Distributer verifies orgin ..."
+# verify origin drug by distributer
+curl 
+
+echo "Login as Pharmacist ..."
+echo "create new bash and run: composer-rest-server (pharmacist_leander@drug_network)"
+read -p
+
+# receive drug from distributer to pharmacist
+
+echo "Distributer verifies orgin ..."
+# verify origin drug by distributer
+curl 
+
+echo ""
+echo "Received drug from Distributer '$distributerId'"
+echo "to Distributer '$pharmacistId'"
+
+echo "Login as Patient ..."
+echo "create new bash and run: composer-rest-server (patient_cedric@drug_network)"
