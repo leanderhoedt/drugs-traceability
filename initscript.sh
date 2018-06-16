09630ad87affd57295f1f328ad1cf15a2b4d352d
Shadak Rahl <cedric.steenbeke@gmail.com>
2018-06-16 19:17:16 +0200

    added different use case to show funcionality in the walktrough script

--- initscript.sh
+++ initscript.sh
@@ -4,13 +4,13 @@
 URI="http://localhost:3000"
 
 echo "Starting Drugs traceability workflow ..."
-composer-rest-server -n 'never' -c admin@drug_network &>/dev/null
+composer-rest-server -n 'never' -c admin@drug_network &
 sleep 5
 echo "composer rest server will be started with admin credentials"
 echo "make sure you started the business network with startBusinessNetwork.sh"
-
+sleep 10
 #composer-rest-server -c admin@drug_network
-#sleep 3
+
 
 echo "2 participants will be created for each type"
 
@@ -22,6 +22,11 @@ manufacturerName="manufacturer_$manufacturerId"
 curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "$class": "org.drugs.Manufacturer", "manufacturerId": "'$manufacturerId'", "name": "Jo Vercammen" }' "$URI/api/Manufacturer"
 echo "Manufacturer [$manufacturerId] created"
 
+manufacturerIdSofie="Sofie"
+manufacturerNameSofie="manufacturer_$manufacturerIdSofie"
+
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "$class": "org.drugs.Manufacturer", "manufacturerId": "'$manufacturerIdSofie'", "name": "Sofie" }' "$URI/api/Manufacturer"
+echo "Manufacturer [$manufacturerIdSofie] created"
 
 # Create distributer
 
@@ -31,6 +36,11 @@ distributerName="distributer_$distributerId"
 curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "$class": "org.drugs.Distributer", "customerId": "'$distributerId'" }' "$URI/api/Distributer"
 echo "Distributer [$distributerId] created"
 
+distributerIdMarijn="marijn"
+distributerNameMarijn="distributer_$distributerIdMarijn"
+
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "$class": "org.drugs.Distributer", "customerId": "'$distributerIdMarijn'" }' "$URI/api/Distributer"
+echo "Distributer [$distributerIdMarijn] created"
  # Create pharmacist
 
 pharmacistId="leander"
@@ -38,6 +48,12 @@ pharmacistName="pharmacist_$pharmacistId"
 
 curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "$class": "org.drugs.Pharmacist", "customerId": "'$pharmacistId'" }' "$URI/api/Pharmacist"
 echo "Pharmacist [$pharmacistId] created"
+
+pharmacistIdMichael="Michael"
+pharmacistNameMichael="pharmacist_$pharmacistIdMichael"
+
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "$class": "org.drugs.Pharmacist", "customerId": "'$pharmacistIdMichael'" }' "$URI/api/Pharmacist"
+echo "Pharmacist [$pharmacistIdMichael] created"
 
 # Create patient
 
@@ -47,27 +63,45 @@ patientName="patient_$patientId"
 curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "$class": "org.drugs.Patient", "customerId": "'$patientId'" }' "$URI/api/Patient"
 echo "Patient [$patientId] created"
 
+patientIdNico="nico"
+patientNameNico="patient_$patientIdNico"
+
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "$class": "org.drugs.Patient", "customerId": "'$patientIdNico'" }' "$URI/api/Patient"
+echo "Patient [$patientIdNico] created"
+
 echo ""
 echo "Creating cards for each participant..."
 echo ""
 # Creating cards of participants
 curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/octet-stream' -d '{ "participant": "resource:org.drugs.Manufacturer#'$manufacturerId'", "userID":"'$manufacturerName'" }' "$URI/api/system/identities/issue" > ${manufacturerName}.card
 echo "Created $manufacturerName.card"
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/octet-stream' -d '{ "participant": "resource:org.drugs.Manufacturer#'$manufacturerIdSofie'", "userID":"'$manufacturerNameSofie'" }' "$URI/api/system/identities/issue" > ${manufacturerNameSofie}.card
+echo "Created $manufacturerNameSofie.card"
 curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/octet-stream' -d '{ "participant": "resource:org.drugs.Distributer#'$distributerId'", "userID":"'$distributerName'" }' "$URI/api/system/identities/issue" > ${distributerName}.card
 echo "Created $distributerName.card"
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/octet-stream' -d '{ "participant": "resource:org.drugs.Distributer#'$distributerIdMarijn'", "userID":"'$distributerNameMarijn'" }' "$URI/api/system/identities/issue" > ${distributerNameMarijn}.card
+echo "Created $distributerNameMarijn.card"
 curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/octet-stream' -d '{ "participant": "resource:org.drugs.Pharmacist#'$pharmacistId'", "userID":"'$pharmacistName'" }' "$URI/api/system/identities/issue" > ${pharmacistName}.card
 echo "Created $pharmacistName.card"
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/octet-stream' -d '{ "participant": "resource:org.drugs.Pharmacist#'$pharmacistIdMichael'", "userID":"'$pharmacistNameMichael'" }' "$URI/api/system/identities/issue" > ${pharmacistNameMichael}.card
+echo "Created $pharmacistNameMichael.card"
 curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/octet-stream' -d '{ "participant": "resource:org.drugs.Patient#'$patientId'", "userID":"'$patientName'" }' "$URI/api/system/identities/issue" > ${patientName}.card
 echo "Created $patientName.card"
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/octet-stream' -d '{ "participant": "resource:org.drugs.Patient#'$patientIdNico'", "userID":"'$patientNameNico'" }' "$URI/api/system/identities/issue" > ${patientNameNico}.card
+echo "Created $patientNameNico.card"
 
 # Import cards of participants
 echo ""
 echo "Importing cards..."
 echo ""
 composer card import -f ./${manufacturerName}.card
+composer card import -f ./${manufacturerNameSofie}.card
 composer card import -f ./${distributerName}.card
+composer card import -f ./${distributerNameMarijn}.card
 composer card import -f ./${pharmacistName}.card
+composer card import -f ./${pharmacistNameMichael}.card
 composer card import -f ./${patientName}.card
+composer card import -f ./${patientNameNico}.card
 echo "Finished importing cards"
 echo ""
 
--- walkthrough.sh
+++ walkthrough.sh
@@ -14,6 +14,201 @@ then sh ./initscript.sh
 fi
 echo "Environment has been setup."
 
+function main(){
+  echo "Note that for the functions to run the init script has to have run once"
+  echo "Please chose the simulation you'd like to run"
+  echo "Press 1 for the happy path (default)"
+  echo "Press 2 for simulating a Distributer trying to create a drug"
+  echo "Press 3 for simulating a Patient trying to sell a drug"
+  echo "Press 4 for simulating a Distributer quarantaining an invalid drug"
+  echo "Press Q/q to quit"
+  read -p "option: " path
+  if [[ $path =~ '2' ]]
+  then
+    evil_distributer
+  elif [[ $path == '3' ]]
+  then 
+    evil_patient
+  elif [[ $path == '4' ]]
+  then 
+    quarantine_drug
+  elif [[ $path =~ ^[Qq]$ ]]
+  then 
+    exit 0
+  else
+    happy_path
+  fi
+}
+
+# Functions 
+function quarantine_drug(){
+  echo "Distributer marijn verifies a drug and this drug has been wrongly created."
+  echo "The drug will be quarantained"
+  # generate sha256 of Codaine metadata
+  codaDrugHash=`sha256sum codaine.json | awk '{ print $1 }'`
+
+  # get metadata of file
+  codaSerialNumber=`readJson codaine.json serialNumber`
+  codaProductCode=`readJson codaine.json productCode`
+  codaBatchNumber=`readJson codaine.json batchNumber`
+
+  manufacturerId="jo"
+  distributerId="marijn"
+  
+  echo "Manufacturer creates medicine with wrong info ... "
+  composer-rest-server -n 'never' -c manufacturer_jo@drug_network &
+  sleep 5
+  read -p "Press any key to continue..."
+  echo "Creating faulty drug ..."
+  #create drug
+  curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "$class": "org.drugs.CreateDrug", "drugHash": "'$codaDrugHash'", "name": "codaine", "metaData": { "$class": "org.drugs.DrugMetaData", "serialNumber": "'$codaSerialNumber'", "productCode": "'$codaProductCode'", "batchNumber": "'$codaBatchNumber'", "manufacturer": "resource:org.drugs.Manufacturer#'$manufacturerId'" } }' "$URI/api/CreateDrug" | python -m json.tool
+  echo ""
+  echo "Created drug '$codaDrugHash'"
+  echo "by Manufacturer '$manufacturerId'"
+  curl -X GET --header 'Accept: application/json' 'http://localhost:3000/api/Drug/'$codaDrugHash | python -m json.tool
+  kill $!
+
+  echo "Login as Distributer Marijn..."
+  composer-rest-server -n 'never' -c distributer_marijn@drug_network &
+  sleep 5
+  read -p "Press any key to continue..."
+  echo ""
+  echo "Distributer verifies orgin ..."
+  curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "$class": "org.drugs.VerifyDrug",  "metaData": { "$class": "org.drugs.DrugMetaData", "serialNumber": "1234-11", "productCode": "'$codaProductCode'", "batchNumber": "'$codaBatchNumber'",  "manufacturer": "resource:org.drugs.Manufacturer#'$manufacturerId'" }, "drugHash": "'$codaDrugHash'", "verifier": "resource:org.drugs.Distributer#'$distributerId'" }' 'http://localhost:3000/api/VerifyDrug' | python -m json.tool
+  echo ""
+  curl -X GET --header 'Accept: application/json' 'http://localhost:3000/api/Drug/'$codaDrugHash | python -m json.tool
+  kill $!
+  
+  main
+}
+function evil_patient(){
+  echo "Patient cedric will try to launch a transaction that sells a drug he ownes to Nico."
+  echo "An exception will be logged saying thay cedric has no right to execute the transaction"
+  # generate sha256 of Dafalgan metadata
+  paraDrugHash=`sha256sum paracetamol.json | awk '{ print $1 }'`
+
+  # get metadata of file
+  paraSerialNumber=`readJson paracetamol.json serialNumber`
+  paraProductCode=`readJson paracetamol.json productCode`
+  paraBatchNumber=`readJson paracetamol.json batchNumber`
+
+  patientId="cedric"
+  patientName="patient_$patientId"
+  patientIdNico="nico"
+  patientNameNico="patient_$patientId"
+
+
+  echo "Login as Patient cedric ... "
+  composer-rest-server -n 'never' -c patient_cedric@drug_network &
+  sleep 5
+  read -p "Press any key to continue..."
+  echo "Selling drug to Nico ..."
+  curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"$class": "org.drugs.ReceiveDrug", "customer": "resource:org.drugs.Patient#'$patientIdNico'", "drug": "resource:org.drugs.Drug#'$paraDrugHash'" }' 'http://localhost:3000/api/ReceiveDrug' | python -m json.tool
+  echo ""
+  read -p "Press any key to finish..."
+  kill $!
+  main
+}
+
+function evil_distributer(){
+  echo "Distributer Gene will try to launch a transaction that creates a drug in Manufacturer Jo's name."
+  echo "An exception will be logged saying thay Gene has no right to execute the transaction"
+  # generate sha256 of Dafalgan metadata
+  aranespDrugHash=`sha256sum Aranesp.json | awk '{ print $1 }'`
+
+  # get metadata of file
+  aranespSerialNumber=`readJson Aranesp.json serialNumber`
+  aranespProductCode=`readJson Aranesp.json productCode`
+  aranespBatchNumber=`readJson Aranesp.json batchNumber`
+
+  manufacturerId="jo"
+  manufacturerName="manufacturer_$manufacturerId"
+  distributerId="gene"
+  distributerName="distributer_$distributerId"
+  pharmacistId="leander"
+  pharmacistName="pharmacist_$pharmacistId"
+  patientId="cedric"
+  patientName="patient_$patientId"
+
+   echo "Login as Distributer ..."
+  composer-rest-server -n 'never' -c distributer_gene@drug_network &
+  sleep 5
+  read -p "Press any key to continue..."
+  echo "Creating drug ..."
+  #create drug
+  curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "$class": "org.drugs.CreateDrug", "drugHash": "'$aranespDrugHash'", "name": "Aranesp", "metaData": { "$class": "org.drugs.DrugMetaData", "serialNumber": "'$aranespSerialNumber'", "productCode": "'$aranespProductCode'", "batchNumber": "'$aranespBatchNumber'", "manufacturer": "resource:org.drugs.Manufacturer#'$manufacturerId'" } }' "$URI/api/CreateDrug" | python -m json.tool
+  echo ""
+  read -p "Press any key to finish..."
+  kill $!
+  main
+}
+
+function happy_path(){
+  # generate sha256 of Dafalgan metadata
+  dafalganDrugHash=`sha256sum Dafalgan.json | awk '{ print $1 }'`
+
+  # get metadata of file
+  dafalganSerialNumber=`readJson Dafalgan.json serialNumber`
+  dafalganProductCode=`readJson Dafalgan.json productCode`
+  dafalganBatchNumber=`readJson Dafalgan.json batchNumber`
+
+  manufacturerId="jo"
+  manufacturerName="manufacturer_$manufacturerId"
+  distributerId="gene"
+  distributerName="distributer_$distributerId"
+  pharmacistId="leander"
+  pharmacistName="pharmacist_$pharmacistId"
+  patientId="cedric"
+  patientName="patient_$patientId"
+
+  echo "Login as Manufacturer ... "
+  composer-rest-server -n 'never' -c manufacturer_jo@drug_network &
+  sleep 5
+  read -p "Press any key to continue..."
+  echo "Creating drug ..."
+  #create drug
+  curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "$class": "org.drugs.CreateDrug", "drugHash": "'$dafalganDrugHash'", "name": "Dafalgan", "metaData": { "$class": "org.drugs.DrugMetaData", "serialNumber": "'$dafalganSerialNumber'", "productCode": "'$dafalganProductCode'", "batchNumber": "'$dafalganBatchNumber'", "manufacturer": "resource:org.drugs.Manufacturer#'$manufacturerId'" } }' "$URI/api/CreateDrug" | python -m json.tool
+  echo ""
+  echo "Created drug '$dafalganDrugHash'"
+  echo "by Manufacturer '$manufacturerId'"
+  curl -X GET --header 'Accept: application/json' 'http://localhost:3000/api/Drug/'$dafalganDrugHash | python -m json.tool
+  kill $!
+
+  echo "Login as Distributer ..."
+  composer-rest-server -n 'never' -c distributer_gene@drug_network &
+  sleep 5
+  read -p "Press any key to continue..."
+  echo ""
+  echo "Distributer verifies orgin ..."
+  curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "$class": "org.drugs.VerifyDrug",  "metaData": { "$class": "org.drugs.DrugMetaData", "serialNumber": "'$dafalganSerialNumber'", "productCode": "'$dafalganProductCode'", "batchNumber": "'$dafalganBatchNumber'",  "manufacturer": "resource:org.drugs.Manufacturer#'$manufacturerId'" }, "drugHash": "'$dafalganDrugHash'", "verifier": "resource:org.drugs.Distributer#'$distributerId'" }' 'http://localhost:3000/api/VerifyDrug' | python -m json.tool
+  echo "Distributer '$distributerId' receives drugs from manufacturer '$manufacturerId'"
+  curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"$class": "org.drugs.ReceiveDrug", "customer": "resource:org.drugs.Distributer#'$distributerId'", "drug": "resource:org.drugs.Drug#'$dafalganDrugHash'" }' 'http://localhost:3000/api/ReceiveDrug' | python -m json.tool
+  echo ""
+  curl -X GET --header 'Accept: application/json' 'http://localhost:3000/api/Drug/'$dafalganDrugHash | python -m json.tool
+  kill $!
+
+  echo "Login as Pharmacist ..."
+  composer-rest-server -n 'never' -c pharmacist_leander@drug_network &
+  read -p "Press any key to continue..."
+  echo ""
+  echo "Pharmacist verifies orgin ..."
+  curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "$class": "org.drugs.VerifyDrug",  "metaData": { "$class": "org.drugs.DrugMetaData", "serialNumber": "'$dafalganSerialNumber'", "productCode": "'$dafalganProductCode'", "batchNumber": "'$dafalganBatchNumber'",  "manufacturer": "resource:org.drugs.Manufacturer#'$manufacturerId'" }, "drugHash": "'$dafalganDrugHash'", "verifier": "resource:org.drugs.Pharmacist#'$pharmacistId'" }' 'http://localhost:3000/api/VerifyDrug' | python -m json.tool
+  echo "Pharmacist '$pharmacistId' receives drugs from distributer '$distributerId'"
+  curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"$class": "org.drugs.ReceiveDrug", "customer": "resource:org.drugs.Pharmacist#'$pharmacistId'", "drug": "resource:org.drugs.Drug#'$dafalganDrugHash'" }' 'http://localhost:3000/api/ReceiveDrug' | python -m json.tool
+  curl -X GET --header 'Accept: application/json' 'http://localhost:3000/api/Drug/'$dafalganDrugHash | python -m json.tool
+  kill $!
+
+  echo "Login as Patient ..."
+  composer-rest-server -n 'never' -c patient_cedric@drug_network &
+  sleep 5
+  read -p "Press any key to continue..."
+  echo ""
+  echo "Patient '$patientId' receives drugs from pharmacist '$pharmacistId'"
+  curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"$class": "org.drugs.ReceiveDrug", "customer": "resource:org.drugs.Patient#'$patientId'", "drug": "resource:org.drugs.Drug#'$dafalganDrugHash'" }' 'http://localhost:3000/api/ReceiveDrug' | python -m json.tool
+  curl -X GET --header 'Accept: application/json' 'http://localhost:3000/api/Drug/'$dafalganDrugHash | python -m json.tool
+  kill $!
+  main
+}
 
 function readJson {  
   UNAMESTR=`uname`
@@ -33,62 +228,5 @@ function readJson {
   fi; 
 }
 
-# generate sha256 of Dafalgan metadata
-dafalganDrugHash=`sha256sum Dafalgan.json | awk '{ print $1 }'`
-
-# get metadata of file
-dafalganSerialNumber=`readJson Dafalgan.json serialNumber`
-dafalganProductCode=`readJson Dafalgan.json productCode`
-dafalganBatchNumber=`readJson Dafalgan.json batchNumber`
-
-manufacturerId="jo"
-manufacturerName="manufacturer_$manufacturerId"
-distributerId="gene"
-distributerName="distributer_$distributerId"
-pharmacistId="leander"
-pharmacistName="pharmacist_$pharmacistId"
-patientId="cedric"
-patientName="patient_$patientId"
-
-echo "Login as Manufacturer ... "
-composer-rest-server -n 'never' -c manufacturer_jo@drug_network &
-sleep 5
-read -p "Press any key to continue..."
-echo "Creating drug ..."
-#create drug
-curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "$class": "org.drugs.CreateDrug", "drugHash": "'$dafalganDrugHash'", "name": "Dafalgan", "metaData": { "$class": "org.drugs.DrugMetaData", "serialNumber": "'$dafalganSerialNumber'", "productCode": "'$dafalganProductCode'", "batchNumber": "'$dafalganBatchNumber'", "manufacturer": "resource:org.drugs.Manufacturer#'$manufacturerId'" } }' "$URI/api/CreateDrug"
-echo ""
-echo "Created drug '$dafalganDrugHash'"
-echo "by Manufacturer '$manufacturerId'"
-kill $!
-
-echo "Login as Distributer ..."
-composer-rest-server -n 'never' -c distributer_gene@drug_network &
-sleep 5
-read -p "Press any key to continue..."
-echo ""
-echo "Distributer verifies orgin ..."
-curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "$class": "org.drugs.VerifyDrug",  "metaData": { "$class": "org.drugs.DrugMetaData", "serialNumber": "'$dafalganSerialNumber'", "productCode": "'$dafalganProductCode'", "batchNumber": "'$dafalganBatchNumber'",  "manufacturer": "resource:org.drugs.Manufacturer#'$manufacturerId'" }, "drugHash": "'$dafalganDrugHash'", "verifier": "resource:org.drugs.Distributer#'$distributerId'" }' 'http://localhost:3000/api/VerifyDrug'
-echo "Distributer '$distributerId' receives drugs from manufacturer '$manufacturerId'"
-curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"$class": "org.drugs.ReceiveDrug", "customer": "resource:org.drugs.Distributer#'$distributerId'", "drug": "resource:org.drugs.Drug#'$dafalganDrugHash'" }' 'http://localhost:3000/api/ReceiveDrug'
-echo ""
-kill $!
-
-echo "Login as Pharmacist ..."
-composer-rest-server -n 'never' -c pharmacist_leander@drug_network &
-read -p "Press any key to continue..."
-echo ""
-echo "Pharmacist verifies orgin ..."
-curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "$class": "org.drugs.VerifyDrug",  "metaData": { "$class": "org.drugs.DrugMetaData", "serialNumber": "'$dafalganSerialNumber'", "productCode": "'$dafalganProductCode'", "batchNumber": "'$dafalganBatchNumber'",  "manufacturer": "resource:org.drugs.Manufacturer#'$manufacturerId'" }, "drugHash": "'$dafalganDrugHash'", "verifier": "resource:org.drugs.Pharmacist#'$pharmacistId'" }' 'http://localhost:3000/api/VerifyDrug'
-echo "Pharmacist '$pharmacistId' receives drugs from distributer '$distributerId'"
-curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"$class": "org.drugs.ReceiveDrug", "customer": "resource:org.drugs.Pharmacist#'$pharmacistId'", "drug": "resource:org.drugs.Drug#'$dafalganDrugHash'" }' 'http://localhost:3000/api/ReceiveDrug'
-kill $!
-
-echo "Login as Patient ..."
-composer-rest-server -n 'never' -c patient_cedric@drug_network &
-sleep 5
-read -p "Press any key to continue..."
-echo ""
-echo "Patient '$patientId' receives drugs from pharmacist '$pharmacistId'"
-curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"$class": "org.drugs.ReceiveDrug", "customer": "resource:org.drugs.Patient#'$patientId'", "drug": "resource:org.drugs.Drug#'$dafalganDrugHash'" }' 'http://localhost:3000/api/ReceiveDrug'
-kill $!
+# MAIN
+main
